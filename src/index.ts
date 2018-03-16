7673d3d79fa32e8776648bbfa02daeeceda906d8
松村和輝 <k.matsumura0314@gmail.com>
2018-03-17 06:12:23 +0900

    insert user

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,42 @@
-console.log('aaa');
+const { createConnection } = require("mysql");
+import { Connection, MysqlError, FieldInfo } from "mysql";
+
+const con: Connection = createConnection({
+  host: "localhost",
+  port: 13306,
+  user: "root",
+  password: "password",
+  database: "sandbox"
+});
+
+const sql = "INSERT INTO users SET ?;";
+
+const createUser = (name: string): Promise<any> => {
+  return new Promise((resolve, reject) => {
+    con.query(sql, { name: name }, (err: MysqlError | null, results: any) => {
+      if (err) {
+        reject(err);
+        return;
+      }
+      resolve(results);
+    });
+  });
+};
+
+new Promise((resolve, reject) => {
+  con.beginTransaction((err: MysqlError) => {
+    if (err) {
+      reject(err);
+      return;
+    }
+    resolve(createUser("zuckey"));
+  });
+})
+  .then(result => {
+    console.log(result);
+    process.exit(0);
+  })
+  .catch((err: Error) => {
+    console.log(err);
+    process.exit(1);
+  });
